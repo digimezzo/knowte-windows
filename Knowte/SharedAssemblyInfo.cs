0f221ba72439cb24048959eee1b1ce58165b0024
Raphaël <raphael.godart@outlook.com>
2016-08-03 21:22:06 +0200

    Updated the changelog

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2016")]
-[assembly: AssemblyVersion("1.1.464.0")]
-[assembly: AssemblyFileVersion("1.1.464.0")]
+[assembly: AssemblyVersion("1.1.465.0")]
+[assembly: AssemblyFileVersion("1.1.465.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.464.0 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.465.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.464.0")]
+[assembly: AssemblyInformationalVersion("1.1.465.0")]
 #endif
