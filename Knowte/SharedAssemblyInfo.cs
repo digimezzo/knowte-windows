238822e8316b2043f80a67851bb0e53d19dd973e
Raphaël <raphael.godart@outlook.com>
2017-01-04 21:28:25 +0100

    Improved the launch combobox on the installer

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2017")]
-[assembly: AssemblyVersion("1.1.0.472")]
-[assembly: AssemblyFileVersion("1.1.0.472")]
+[assembly: AssemblyVersion("1.1.0.473")]
+[assembly: AssemblyFileVersion("1.1.0.473")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.0.472 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.0.473 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.0.472")]
+[assembly: AssemblyInformationalVersion("1.1.0.473")]
 #endif
