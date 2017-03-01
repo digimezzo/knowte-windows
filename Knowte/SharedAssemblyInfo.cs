7bda62e778c9fb68a4444a9b525ac50ed1f4410b
Raphael <raphael@DESKTOP-64I0SI3>
2017-03-01 13:24:33 +0100

    Updated change log, made busy window not movable

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2017")]
-[assembly: AssemblyVersion("1.1.0.474")]
-[assembly: AssemblyFileVersion("1.1.0.474")]
+[assembly: AssemblyVersion("1.1.0.475")]
+[assembly: AssemblyFileVersion("1.1.0.475")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.0.474 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.0.475 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.0.474")]
+[assembly: AssemblyInformationalVersion("1.1.0.475")]
 #endif
