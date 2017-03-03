0ba84d99ff369529349040f10eb65a36e1fc6cb4
Raphael <raphael@DESKTOP-64I0SI3>
2017-03-03 11:24:27 +0100

    Removed separators from Settings screens

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2017")]
-[assembly: AssemblyVersion("1.1.0.477")]
-[assembly: AssemblyFileVersion("1.1.0.477")]
+[assembly: AssemblyVersion("1.1.0.478")]
+[assembly: AssemblyFileVersion("1.1.0.478")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.0.477 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.0.478 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.0.477")]
+[assembly: AssemblyInformationalVersion("1.1.0.478")]
 #endif
