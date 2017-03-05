b1588cd5775a87bb90872df1c87b8f3fbc01ef81
Raphaël <raphael.godart@outlook.com>
2017-03-05 01:11:07 +0100

    Fixed a bug deleting notebooks

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2017")]
-[assembly: AssemblyVersion("1.1.0.478")]
-[assembly: AssemblyFileVersion("1.1.0.478")]
+[assembly: AssemblyVersion("1.1.0.479")]
+[assembly: AssemblyFileVersion("1.1.0.479")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.0.478 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.0.479 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.0.478")]
+[assembly: AssemblyInformationalVersion("1.1.0.479")]
 #endif
