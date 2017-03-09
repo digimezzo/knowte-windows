da675e721ee45ba9e147844dd8990c9ddc21a13a
Raphael <raphael@DESKTOP-64I0SI3>
2017-03-09 15:06:57 +0100

    Fixed multiple issues

--- Knowte.Common/Base/Constants.cs
+++ Knowte.Common/Base/Constants.cs
@@ -4,6 +4,7 @@
     {
         // Fonts
         public static double SplashAppNameFontSize = 44;
+        public static double AboutAppNameFontSize = 48;
 
         // Transparency
         public static readonly double OpacityWhenBlurred = 0.85;
--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2017")]
-[assembly: AssemblyVersion("1.1.0.487")]
-[assembly: AssemblyFileVersion("1.1.0.487")]
+[assembly: AssemblyVersion("1.1.0.488")]
+[assembly: AssemblyFileVersion("1.1.0.488")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.0.487 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.0.488 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.0.487")]
+[assembly: AssemblyInformationalVersion("1.1.0.488")]
 #endif
