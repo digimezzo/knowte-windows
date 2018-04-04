da0b4dbfad33638446fba2a62a5f982e9ecad034
Raphaël <raphael.godart@outlook.com>
2018-04-04 14:15:17 +0200

    Bumped assembly version

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2018")]
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
-[assembly: AssemblyInformationalVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
+[assembly: AssemblyInformationalVersion("1.1.6.0")]
