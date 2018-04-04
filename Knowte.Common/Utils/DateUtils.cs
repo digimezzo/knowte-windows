33c51c6a097f0cde1dfda051be5fcddc8f9face4
Raphaël <raphael.godart@outlook.com>
2018-04-04 14:13:47 +0200

    Added more fuzzy dates

--- Knowte.Common/Utils/DateUtils.cs
+++ Knowte.Common/Utils/DateUtils.cs
@@ -48,6 +48,46 @@ namespace Knowte.Common.Utils
                 {
                     retVal = "Last month";
                 }
+                else if (endTime.Month == startTime.Month + 2 & endTime.Year == startTime.Year)
+                {
+                    retVal = "2 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 3 & endTime.Year == startTime.Year)
+                {
+                    retVal = "3 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 4 & endTime.Year == startTime.Year)
+                {
+                    retVal = "4 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 5 & endTime.Year == startTime.Year)
+                {
+                    retVal = "5 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 6 & endTime.Year == startTime.Year)
+                {
+                    retVal = "6 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 7 & endTime.Year == startTime.Year)
+                {
+                    retVal = "7 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 8 & endTime.Year == startTime.Year)
+                {
+                    retVal = "8 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 9 & endTime.Year == startTime.Year)
+                {
+                    retVal = "9 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 10 & endTime.Year == startTime.Year)
+                {
+                    retVal = "10 months ago";
+                }
+                else if (endTime.Month == startTime.Month + 11 & endTime.Year == startTime.Year)
+                {
+                    retVal = "11 months ago";
+                }
                 else
                 {
                     retVal = "Long ago";
