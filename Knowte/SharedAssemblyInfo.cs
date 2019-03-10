27b4e1c99866332f47d9d41d4840c13228771653
Raphaël <raphael@digimezzo.com>
2019-03-10 16:53:40 +0100

    Updated change log and assembly info

--- Knowte/SharedAssemblyInfo.cs
+++ Knowte/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Digimezzo")]
-[assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2018")]
-[assembly: AssemblyVersion("1.1.6.0")]
-[assembly: AssemblyFileVersion("1.1.6.0")]
-[assembly: AssemblyInformationalVersion("1.1.6.0")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2013 - 2019")]
+[assembly: AssemblyVersion("1.1.7.0")]
+[assembly: AssemblyFileVersion("1.1.7.0")]
+[assembly: AssemblyInformationalVersion("1.1.7.0")]
